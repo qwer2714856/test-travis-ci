0942df62142f0357bd8d89dfc39ba6f3d8246869
ding.luchao <ding.luchao@theadventus.com>
2021-06-08 08:23:39 +0000

    blog.jzxer.cn

---
+++ precache-manifest.97cbbbb4ffd03d69d429c44fc77765b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42be39ca1ac77218f1a19813ca903d4",
+    "url": "https://cdn.jsdelivr.net/gh/qwer2714856/test-travis-ci@gh-pages/index.html"
+  },
+  {
+    "revision": "b69209f9a6b3752d979f",
+    "url": "https://cdn.jsdelivr.net/gh/qwer2714856/test-travis-ci@gh-pages/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "5a46617966b536919ab2",
+    "url": "https://cdn.jsdelivr.net/gh/qwer2714856/test-travis-ci@gh-pages/static/js/2.68f56c50.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "https://cdn.jsdelivr.net/gh/qwer2714856/test-travis-ci@gh-pages/static/js/2.68f56c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b69209f9a6b3752d979f",
+    "url": "https://cdn.jsdelivr.net/gh/qwer2714856/test-travis-ci@gh-pages/static/js/main.312a0bdb.chunk.js"
+  },
+  {
+    "revision": "0c665a49d339afe77a02",
+    "url": "https://cdn.jsdelivr.net/gh/qwer2714856/test-travis-ci@gh-pages/static/js/runtime-main.2aa809d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://cdn.jsdelivr.net/gh/qwer2714856/test-travis-ci@gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
